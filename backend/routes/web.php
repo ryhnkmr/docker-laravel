0df498105248782c930e7a09be3303881a3dcf43
Kojiro <kn.kojiro@gmail.com>
2020-12-16 21:15:21 +0900

    move barcode_reader to the view folder
    htmlをblade.phpに変更（JSは切り離しまだ）
    ルーティングを変更

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create_characters', function () {
+    return view('create_characters');
+});
