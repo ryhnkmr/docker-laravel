2200300d64568e10bbcaf596c1534ce08630fa8f
chiharu <chiharu_1120@hotmail.com>
2020-12-17 18:25:55 +0900

    Mypage作成
    仮完成

--- backend/resources/views/mypage.blade.php
+++ backend/resources/views/mypage.blade.php
@@ -3,79 +3,125 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>mypage</title>
+     <title>MyPage</title>
      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-     <link href="{{ asset('css/mypage.css')}}" rel="stylesheet">
+
 </nav>
 </head>
 
 <style>
     body{
-        color:#73879c; background:#f7f7f7;
+        color:#6c757d; background:#f7f7f7;
     }
 </style>
 <body>
-    <div class="container-fluid">
+    <div class="container">
         
-            <!-- top -->
+<!-- top -->
             <div class="">
                 <nav class="navbar navbar-light bg-light" >
-                    <p class="navbar-brand">TITLE</p>
+                    <h1 class="navbar-brand">TITLE</h1>
                 </nav>
             </div>
 
         <div class="row"> 
-            <!-- left -->
-                <div class="col-sm-3 col-lg-2 sidebar bg-warning"> 
-                    <!--ユーザーアイコン-->
-                    <div class="profile-sidebar"> 
-                        <img src="http://placehold.it/50/30a5ff/fff" class="img-responsive">
-                    </div>
+<!-- left -->
+                <div class="col-xl-3 col-sm-4 sidebar bg-warning "> 
 
-                    <!--ユーザーネーム-->
-                    <div class="profile-user-title"> 
-                        <div class="profile-usertitle-name">Username</div>
+                        <!--ユーザーアイコンとユーザーネーム-->
+                    <div class="usericonname d-flex justify-content-center ">
+                            <div class="profile-usertitle-name">
+                                <img src="https://placehold.jp/100x100.png" alt="" class="m-3">
+                                <h3 class="font-weight-bold">Username</h3>
+                            </div>
                     </div>
 
                     <!--戦績-->
-                    <div class="profile-user-pointrrecord">
-                        <div class="profile-user-pointtitle">POINTS</div>
-                        <div class="profile-user-point">1800pt</div>
+                    <div class="profile-user-pointrrecord font-weight-bold">
+                        <div class="profile-user-pointtitle ">POINTS</div>
+                        <div class="profile-user-point col-8 border border-dark bg-white mx-auto m-2">
+                            <p class="d-flex align-items-center justify-content-center">echo 1800.'Pt';</p>  
+                        </div>
                         <div class="profile-user-recordtitle">Battle Record</div>
-                        <div class="profile-user-record">5win 3lose</div>
+                        <div class="profile-user-record col-8 border border-dark bg-white mx-auto m-2">
+                            <p class="text-center">5win 3lose</p>
+                        </div>
                     </div>
                 </div>
 
-            <!-- right -->
-            <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-log-offset-2 main">
-                
-                    <div class="main-operation bg-danger"><!--ゲームのメニュー-->
-                        <div class="row"> <!--準備-->
-                            <div class="">CharacterSet</div>
-                            <div class="">BattleRoom</div>
-                            <div class="">BetRoom</div>
+<!-- right -->
+            <div class="col-xl-9 col-sm-8 main">
+                    <!--ゲームのメニュー-->
+                    <div class="main-operation ">
+                        <!--準備-->
+                        <div class="row my-4"> 
+                           
+                                <div class="RoomSet mx-auto">
+                                    <button type="button" class="btn btn-primary btn-md ">CharacterSet</button>    
+                                </div>
+
+                                <div class="RoomSet mx-auto">
+                                    <button type="button" class="btn btn-primary btn-md">BattleRoom</button>    
+                                </div>
+
+                                <div class="RoomSet mx-auto">
+                                    <button type="button" class="btn btn-primary btn-md">BetRoom</button>
+                                </div>
+                            
                         </div>
-                        <div class="main-todaysNews">
-                        Today's News
+
+                        <div class="main-todaysNews bg-white text-dark">
+                            <!--ニュース-->
+                            <div class="m-2">
+                                <ul>
+                                    <p>Today's News</p>
+                                    <li>テストテスト</li>
+                                    <li>テストテスト</li>
+                                    <li>テストテスト</li>
+                                </ul>
+                             </div>
                         </div>
                     </div>
                     <div class="main-team bg-info">
-                        <div class="main-team-starting"> <!--スターティングメンバ―-->
-                            <div class="main-team-yourteam">Your Team</div>
-                            <div class="row">
-                                <div class="">1st</div>
-                                <div class="">2nd</div>
-                                <div class="">3rd</div>
+                        <!--スターティングメンバ―-->
+                        <div class="main-team-starting my-2"> 
+                            <div class="main-team-yourteam ">
+                                <h3 class="p-4 font-weight-bold">YOUR TEAM</h3>
+                                    <div class="row col-8 mx-auto">
+                                        <div class="m-3">
+                                            <img src="https://placehold.jp/140x200.png" alt="">
+                                        </div>
+                                        <div class="m-3">
+                                            <img src="https://placehold.jp/140x200.png" alt="">
+                                        </div>
+                                        <div class="m-3">
+                                            <img src="https://placehold.jp/140x200.png" alt="">
+                                        </div>
+                                    </div>
                             </div>
                         </div>
                         <div class="main-team-stock"> <!--ストック-->
-                            <div class="main-team-stock-name">Stock CharactorsStock</div>
-                            <div class="row">
-                                <div class=""></div>
-                                <div class=""></div>
-                                <div class=""></div>
-                                <div class=""></div>
-                                <div class=""></div>
+                            <div class="main-team-stock-name sm-5">
+                                <h3 class="p-4 font-weight-bold">Stock CharactorsStock</h3>
+                                    <tr>
+                                        <div class="row col-8 mx-auto">
+                                            <div class="m-3">
+                                                <img src="https://placehold.jp/70x70.png" alt="">
+                                            </div>
+                                            <div class="m-3">
+                                                <img src="https://placehold.jp/70x70.png" alt="">
+                                            </div>
+                                            <div class="m-3">
+                                                <img src="https://placehold.jp/70x70.png" alt="">
+                                            </div>
+                                            <div class="m-3">
+                                                <img src="https://placehold.jp/70x70.png" alt="">
+                                            </div>
+                                            <div class="m-3">
+                                                <img src="https://placehold.jp/70x70.png" alt="">
+                                            </div>
+                                        </div>
+                                    </tr>   
                             </div>
                         </div>
                     </div>
@@ -83,5 +129,6 @@
             </div>
         </div>
     </div>
+    <script src="/js/app.js"></script>
 </body>
 </html>
