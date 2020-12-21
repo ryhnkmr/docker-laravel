dc724fb8df322c9b5ee831eaf2d1de86983bf617
chiharu <chiharu_1120@hotmail.com>
2020-12-21 23:38:45 +0900

    TOPPAGE
    コンテナ組んだ

---
+++ backend/resources/views/toppage.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🐤ブックdeバーコードバトラー📚</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">a</div>
+            <div class="col-md-4">i</div>
+            <div class="col-md-4">u</div>
+        </div>
+    
+</div>
+
+
+
+
+
+
+
+
+        <!-- <div>
+            @if (Route::has('login'))
+                <div>
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif -->
+</html>
