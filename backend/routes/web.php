54f6754a3b190bd5b1b900d41acacb1701a128b5
Kojiro <kn.kojiro@gmail.com>
2020-12-22 12:17:55 +0900

    home画面途中

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\RoomController;
-
+use App\Models\Team;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,26 @@ use App\Http\Controllers\RoomController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::post('/teams', function (Request $request) {
+    // Eloquent モデル
+    // $teams = new Team;
+    // $books->item_name =    $request->item_name;
+    // $books->item_number =  $request->item_number;
+    // $books->item_amount =  $request->item_amount;
+    // $books->published =    $request->published;
+    // $books->save();   //「/」ルートにリダイレクト 
+    return redirect('/');
+    $team = Team::create([
+    //     'name' => $request->name,
+    //     'user_id' => $request->user_id,
+    ]);
+});
+
+Route::get('/home', function () {
+    return view('index');
+});
+
 
 Route::get('/', function () {
     return view('index');
